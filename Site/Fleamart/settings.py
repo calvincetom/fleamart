002b045c4c26b13cdd491d2ff00ffb41f10480f9
calvincetom <calvincetom@outlook.com>
2023-05-16 14:34:27 +0300

    create cart functionality and manage user sessions

--- Site/Fleamart/settings.py
+++ Site/Fleamart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Main.apps.MainConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR /'media'
+CART_SESSION_ID = "cart"
--- Site/Main/views.py
+++ Site/Main/views.py
@@ -15,5 +15,5 @@ def product_list(request, category_slug=None):
     })
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(product, id=id, slug=slug, available=True)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'Main/product/detail.html', {'product':product})
